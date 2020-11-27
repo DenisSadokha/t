56b15ba311418107b94744df3f726a688df41515
DenisSadokha <71551573+DenisSadokha@users.noreply.github.com>
2020-11-27 11:52:27 +0700

    Add files via upload

---
+++ todo/src/config.js
@@ -0,0 +1,11 @@
+  export const requestApi = {
+    editApi :'http://localhost:3001/api/tasks/',
+    getApi : "http://localhost:3001/api/tasks?access_token=",
+    deleteApi : "http://localhost:3001/api/tasks/",
+    createApi : "http://localhost:3001/api/tasks?access_token=",
+    loginApi :"http://localhost:3001/api/Users/login",
+    registrApi : 'http://localhost:3001/api/Users',
+
+    
+  }
+
