0448da1f1bf3c383c89d14de96888b6a9f391d68
DenisSadokha <71551573+DenisSadokha@users.noreply.github.com>
2020-11-27 11:54:31 +0700

    Update StoreArticle.js

--- todo/src/store/StoreArticle.js
+++ todo/src/store/StoreArticle.js
@@ -3,35 +3,33 @@ import { action, observable, decorate } from "mobx";
 import { Redirect } from "react-router"
 import makeRequestForUpdate from "../requestHelper/requestForGet";
 import StoreNotes from "./StoreNotes"
-let  editApi ='http://localhost:3001/api/tasks/';
-let getApi = "http://localhost:3001/api/tasks?access_token="
-let deleteApi = "http://localhost:3001/api/tasks/"
-class StoreArticle{
-   openEditor(title, body, token, id, done){
-  edit.setProps(title, body, token, id, done);
+import { requestApi } from "../config"
+class StoreArticle {
+   openEditor(title, body, token, id, done) {
+      edit.setProps(title, body, token, id, done);
 
    }
-   completeTask(title, body, token, id, done){
-      let url = editApi+id+"?access_token="+token;
+   completeTask(title, body, token, id, done) {
+      let url = requestApi.editApi + id + "?access_token=" + token;
       let form = {
          title: title,
          body: body,
          done: done,
          id: id
-     }
-      makeRequestForUpdate(url, form, "PUT", getApi+token, true);
+      }
+      makeRequestForUpdate(url, form, "PUT", requestApi.getApi + token, true);
 
    }
-   deleteTask(id, token){
-      let url = deleteApi+id+"?access_token="+token;
-         makeRequestForUpdate(url, null, 'DELETE', getApi+token, true);
+   deleteTask(id, token) {
+      let url = requestApi.deleteApi + id + "?access_token=" + token;
+      makeRequestForUpdate(url, null, 'DELETE', requestApi.getApi + token, true);
 
    }
 
 
 }
 StoreArticle = decorate(StoreArticle, {
-   
- 
- })
- export default new StoreArticle();
+
+
+})
+export default new StoreArticle();
