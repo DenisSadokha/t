42c763eb5666c90af228e0eb3154bf4656cf7d4a
DenisSadokha <71551573+DenisSadokha@users.noreply.github.com>
2020-11-27 11:53:26 +0700

    Update StoreNotes.js

--- todo/src/store/StoreNotes.js
+++ todo/src/store/StoreNotes.js
@@ -3,13 +3,12 @@ import makeRequest from "../requestHelper/request"
 import { action, observable, decorate, computed, makeA } from "mobx";
 import StoreCreate from "./StoreCreate"
 
-let regApi = "http://localhost:3001/api/tasks?access_token="
-
 class StoreNotes {
     arrTask = [];
     userId;
     token;
     isLoading;
+    isAuth;
     clear() {
         this.arrTask = [];
 
@@ -25,42 +24,17 @@ class StoreNotes {
     update(val) {
         this.arrTask = val.concat();
     }
-    setToken(token){
-        StoreCreate.setTokenVal(token)
-    }
-    setAuth(auth, token, arrTask) {
-        console.log("AUTHOR" + auth)
-        console.log(token)
-        this.token = token;
-        this.auth = auth;
-        this.arrTask = arrTask;
 
-    }
-
-    get allTask() {
-        return this.arrTask;
-    }
 
 
     StoreNotes = decorate(StoreNotes, {
-        auth: observable,
-        // userId: observable,
-        setAuth: action,
-        status: observable,
-        makeRequests: action,
-        token: observable,
+        isAuth: observable,
         arrTask: observable,
-        allTask: computed,
         update: action,
         clear: action,
         isLoading: observable,
         loadStart: action,
         loadComplete: action,
-
-
-
-        // request: action,
-
     })
 
 }
